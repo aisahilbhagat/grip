268edf6b334c4986f3d35ca0902dbb42746dd40e
aisahilbhagat <sahilbhagat9576@gmail.com>
2026-01-05 18:44:16 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyADtCEmADN8ueIwBb91mohYv5eiL9plgz0",
+    authDomain: "yushi-login.firebaseapp.com",
+    projectId: "yushi-login",
+    storageBucket: "yushi-login.firebasestorage.app",
+    messagingSenderId: "75893637403",
+    appId: "1:75893637403:web:9f8fd7954714ecf01f13be",
+    measurementId: "G-58ZDG54Z4P"
+};
